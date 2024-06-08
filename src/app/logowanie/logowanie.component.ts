1494104acf5215a88f342d2b2a318e25e936db5b
i.pondo <pondoizabela@gmail.com>
2024-06-09 00:19:52 +0200

    utworzenie apk

---
+++ src/app/logowanie/logowanie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logowanie',
+  standalone: true,
+  imports: [],
+  templateUrl: './logowanie.component.html',
+  styleUrl: './logowanie.component.css'
+})
+export class LogowanieComponent {
+
+}
